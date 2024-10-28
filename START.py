9af34c320b9faeac6bf01f17a1e4ee9e0e0d7dcf
Prospero1988 <arek.kein@gmail.com>
2024-10-28 14:56:42 +0100

    gui update

--- START.py
+++ START.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk
 import subprocess
 import sys
 import os
+import webbrowser  # Import webbrowser to open URLs
 
 # Tooltip class
 class ToolTip:
@@ -69,17 +70,20 @@ def open_example_file():
         messagebox.showerror("Error", f"Could not open the file: {e}")
 
 def load_example_as_input():
-    # Set the CSV path variable to the example file path
     example_file_path = os.path.join(os.getcwd(), "input_example.csv")
     if os.path.exists(example_file_path):
         csv_path_var.set(example_file_path)
     else:
         messagebox.showerror("Error", "Example file not found.")
 
+def open_help():
+    # Open the GitHub repository in the default web browser
+    webbrowser.open("https://github.com/Prospero1988/logD_predictor")
+
 # Initialize GUI
 root = tk.Tk()
 root.title("logD Predictor GUI")
-root.geometry("400x560")  # Adjusted window height to fit logo
+root.geometry("400x580")  # Adjusted window height to fit logo
 
 path_to_logo = os.path.join(os.getcwd(), "IMG", "LOGO.png")
 # Load and display the logo image
@@ -95,24 +99,29 @@ except Exception as e:
 
 # CSV file path
 csv_path_var = tk.StringVar()
-tk.Label(root, text="Path to CSV file:").pack(pady=5, anchor="w")
+tk.Label(root, text="SELECT .CSV INPUT FILE:").pack(pady=5, anchor="center")
 tk.Entry(root, textvariable=csv_path_var, width=50).pack()
-tk.Button(root, text="Select File", command=select_csv).pack(pady=5)
 
-# Frame for example buttons
+# Define the Select File button
+select_file_button = tk.Button(root, text="Select File", command=select_csv)
+select_file_button.pack(pady=5)
+
+# Frame for example buttons, centered in the main window, with a fixed 10-pixel padding
 example_frame = tk.Frame(root)
-example_frame.pack(anchor="w", pady=5)
+example_frame.pack(anchor="center", pady=20)  # Offset by a fixed 10 pixels
 
-# Button to open example file
+# Buttons with even padding in between
 example_button = tk.Button(example_frame, text="Input File Example", command=open_example_file)
-example_button.pack(side="left", padx=(0, 5))
+example_button.pack(side="left", padx=(5, 5))
 
-# Button to load example file as input
-load_example_button = tk.Button(example_frame, text="Load Input Example", command=load_example_as_input)
-load_example_button.pack(side="left")
+load_example_button = tk.Button(example_frame, text="Start with Input Example", command=load_example_as_input)
+load_example_button.pack(side="left", padx=(5, 5))
+
+help_button = tk.Button(example_frame, text="Open Help", command=open_help)
+help_button.pack(side="left", padx=(5, 5))
 
 # Predictor selection with aliases
-tk.Label(root, text="Select predictor:").pack(pady=5, anchor="w")
+tk.Label(root, text="SELECT PREDICTOR MODEL:").pack(pady=5, anchor="center")
 predictor_var = tk.StringVar(value="all")
 
 # Define options and aliases
@@ -128,7 +137,7 @@ for value, alias in predictor_options.items():
     tk.Radiobutton(root, text=alias, variable=predictor_var, value=value).pack(anchor="w")
 
 # Script execution options
-tk.Label(root, text="Script execution options:").pack(pady=5, anchor="w")
+tk.Label(root, text="SCRIPT EXECUTION OPTIONS:").pack(pady=5, anchor="center")
 debug_var = tk.BooleanVar()
 models_var = tk.BooleanVar()
 quiet_var = tk.BooleanVar(value=True)  # Set quiet mode to be checked by default
@@ -144,7 +153,7 @@ ToolTip(models_button, "Display Machine Learning model details in the output.")
 
 quiet_button = tk.Checkbutton(root, text="Quiet mode", variable=quiet_var)
 quiet_button.pack(anchor="w")
-ToolTip(quiet_button, "Suppress non-essential output messages. Default option to use.")
+ToolTip(quiet_button, "Suppress non-essential output messages. Default option.")
 
 # Button to run the script
 tk.Button(root, text="START Predictions", command=run_script).pack(pady=20)
